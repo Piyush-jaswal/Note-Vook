2a5bf6b05f49d1e11e20287e0b7b9c7352fdcc7d
Piyush <piyushj0809@gmail.com>
2023-04-18 18:30:38 +0530

    Second commit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";//mongodb://localhost:27017 
+const mongoURI = "mongodb+srv://piyushjas:password10@cluster0.ndckwam.mongodb.net/?retryWrites=true&w=majority";//mongodb://localhost:27017 
 mongoose.set('strictQuery', true);
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI,()=>{
